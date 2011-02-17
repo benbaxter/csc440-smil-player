383030fb2f791fba57bbc429aa917c79b83c4ede
knghtmr200 <knghtmr200@1cece00b-e805-e762-627c-26ccc9fbb8d9>
2011-02-17 21:21:30 +0000

    Now adds an image to the canvas when you click the add button. The image responds to touch, but it can't be moved and I don't know why. Reference Drag n Drop.

--- sandbox/UI_test_cord-bris/src/com/team1/ui/UI.java
+++ sandbox/UI_test_cord-bris/src/com/team1/ui/UI.java
@@ -4,41 +4,63 @@ package com.team1.ui;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.Toast;
-
+import android.widget.FrameLayout.LayoutParams;
 
 public class UI extends Activity {
     /** Called when the activity is first created. */
 	Toast toast;
+	View pawn;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        pawn = new View(this);
+		
         setContentView(R.layout.main);
+        
         Button add = (Button)findViewById(R.id.addBtn);
         Button save = (Button)findViewById(R.id.saveBtn);
         Button undo = (Button)findViewById(R.id.undoBtn);
         Button send = (Button)findViewById(R.id.sendBtn);
         Button newBtn = (Button)findViewById(R.id.newBtn);
+        
+        
         add.setOnClickListener(mClick);
         save.setOnClickListener(mClick);
         undo.setOnClickListener(mClick);
         send.setOnClickListener(mClick);
         newBtn.setOnClickListener(mClick);
+        
+        
     }
     
     OnClickListener mClick = new OnClickListener() {
+    	@Override
     	public void onClick(View v){    	
-    		//toast.cancel();
     		Context context = getApplicationContext();
     		toast = Toast.makeText(context, "", Toast.LENGTH_SHORT);
     		if(v.getId() == R.id.addBtn)
     		{
-    			toast.setText("Clicking this button allows the user to add new objects to the Canvas.");
-    			toast.show();
+    			FrameLayout fl = (FrameLayout)findViewById(R.id.Canvas); 
+    			LayoutInflater inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+    			View itemView = inflater.inflate(R.layout.image_add, null);
+    			
+    			View image = (View)itemView.findViewById(R.id.image);   
+                image.setBackgroundResource(R.drawable.icon); 
+                
+    			fl.addView(itemView, new FrameLayout.LayoutParams(40, 40));  
+    			
+    			pawn = findViewById(R.id.image);
+    			pawn.setOnTouchListener(drag);
     		}
     		else if(v.getId() == R.id.saveBtn)
     		{
@@ -63,4 +85,39 @@ public class UI extends Activity {
 
     	}
     };
+    
+    OnTouchListener drag = new OnTouchListener(){
+		@Override 
+		public boolean onTouch(View v, MotionEvent event){ 
+			FrameLayout.LayoutParams par = (LayoutParams) v.getLayoutParams(); 
+			switch(v.getId()){//What is being touched 
+				case R.id.image:{//Which action is being taken 
+					switch(event.getAction()){ 
+						case MotionEvent.ACTION_MOVE:{ 	
+							par.topMargin = (int)event.getRawY() - (v.getHeight()); 
+							par.leftMargin = (int)event.getRawX() - (v.getWidth()/2); 
+							v.setLayoutParams(par); 
+							break; 
+						}//inner case MOVE 
+						case MotionEvent.ACTION_UP:{ 
+							par.height = 40; 
+							par.width = 40; 
+							par.topMargin = (int)event.getRawY() - (v.getHeight()); 
+							par.leftMargin = (int)event.getRawX() - (v.getWidth()/2); 
+							v.setLayoutParams(par); 
+							break; 
+						}//inner case UP 
+						case MotionEvent.ACTION_DOWN:{ 
+							par.height = 60; 
+							par.width = 60; 
+							v.setLayoutParams(par); 
+							break; 
+						}//inner case UP 
+					}//inner switch 
+					break; 
+				}//case image
+			}//switch 
+			return true; 
+		}//onTouch 
+	};//drag
 }
