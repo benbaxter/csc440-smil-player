79e785a863c87bb3044885594962862a92c4f2b4
niklasj1@nku.edu <niklasj1@nku.edu@1cece00b-e805-e762-627c-26ccc9fbb8d9>
2011-03-30 04:10:06 +0000

    Added .smil file browsers for draft, inbox, and outbox

---
+++ dev/SMIL Project/src/com/team1/composer/FileBrowserAdapter.java
@@ -0,0 +1,40 @@
+package com.team1.composer;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class FileBrowserAdapter extends ArrayAdapter<String> 
+{
+	private Activity mContext;
+	private ArrayList<String> mItems;
+	
+	public FileBrowserAdapter ( Activity context, int textViewResourceId, ArrayList<String> items ) 
+	{
+        super ( context, textViewResourceId, items );
+        this.mContext = context;
+        this.mItems = items;
+	}
+	
+	@Override
+    public View getView ( int position, View convertView, ViewGroup parent ) 
+	{
+		View view = convertView;
+		if ( view == null ) 
+		{
+            LayoutInflater vi = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = vi.inflate ( android.R.layout.simple_list_item_1, null );
+        }
+		
+		TextView text = (TextView)view.findViewById ( android.R.id.text1 );
+		text.setText ( mItems.get ( position ) );
+		
+		return view;
+	}
+}
