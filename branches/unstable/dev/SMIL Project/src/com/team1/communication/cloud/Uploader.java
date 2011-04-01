5cd4233c1e032684d83ccd5a3f6141351c12ed12
simonzeno@gmail.com <simonzeno@gmail.com@1cece00b-e805-e762-627c-26ccc9fbb8d9>
2011-04-01 23:19:05 +0000

    Cloud Stuff uploaded

---
+++ branches/unstable/dev/SMIL Project/src/com/team1/communication/cloud/Uploader.java
@@ -0,0 +1,29 @@
+package com.team1.communication.cloud;
+import java.io.*;
+import java.net.*;
+
+
+public class Uploader {
+
+	public static boolean upload(String urp, File file)
+	{
+		String uploadURL = HTTPRequestPoster.sendGetRequest(urp, "");
+		try {
+			ClientHttpRequest.post(new URL(uploadURL), new Object[] {"myFile", file });
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+}
