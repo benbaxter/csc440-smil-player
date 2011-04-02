1b87d87a3ceb11679496b01935e7eadf0f097c86
baxterb89@gmail.com <baxterb89@gmail.com@1cece00b-e805-e762-627c-26ccc9fbb8d9>
2011-04-02 00:59:15 +0000

    adding cloud/CloudConstants.java

---
+++ dev/SMIL Project/src/com/team1/communication/cloud/CloudConstants.java
@@ -0,0 +1,7 @@
+package com.team1.communication.cloud;
+
+public class CloudConstants
+{
+    public static final String uploadURL = "http://cscteamxuploadtest.appspot.com/anupload";
+    public static final String downloadURL = "http://cscteamxuploadtest.appspot.com/andownload";
+}
