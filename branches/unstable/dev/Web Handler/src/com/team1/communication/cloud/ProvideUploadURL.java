4cacdd9365094e13c59d5798f4ece87e8c8635c8
simonzeno@gmail.com <simonzeno@gmail.com@1cece00b-e805-e762-627c-26ccc9fbb8d9>
2011-04-03 05:23:45 +0000

    web handler upload

---
+++ branches/unstable/dev/Web Handler/src/com/team1/communication/cloud/ProvideUploadURL.java
@@ -0,0 +1,36 @@
+package com.team1.communication.cloud;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+
+public class ProvideUploadURL extends HttpServlet {
+    private BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+    	throws ServletException, IOException
+    {
+    	PrintWriter out = res.getWriter();
+    	String url = blobstoreService.createUploadUrl("/anupload");
+    	res.setContentType("text/plain");
+    	out.println(url);
+    }
+    
+    public void doPost(HttpServletRequest req, HttpServletResponse res)
+    	throws ServletException, IOException
+    {
+    	PrintWriter out = res.getWriter();
+    	String url = blobstoreService.createUploadUrl("/anupload");
+    	res.setContentType("text/plain");
+    	out.println(url);
+    }
+}
