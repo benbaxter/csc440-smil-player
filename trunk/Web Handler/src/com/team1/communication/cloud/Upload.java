b0f595b7c02601f278f369a2b2edc598adf1a5f8
baxterb89@gmail.com <baxterb89@gmail.com@1cece00b-e805-e762-627c-26ccc9fbb8d9>
2011-04-23 22:42:42 +0000

    Moving code from Dev to trunk

---
+++ trunk/Web Handler/src/com/team1/communication/cloud/Upload.java
@@ -0,0 +1,28 @@
+package com.team1.communication.cloud;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+
+public class Upload extends HttpServlet {
+    private BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res)
+        throws ServletException, IOException {
+
+        Map<String, BlobKey> blobs = blobstoreService.getUploadedBlobs(req);
+        BlobKey blobKey = blobs.get("myFile");
+        PrintWriter out = res.getWriter();
+        out.println(blobKey.getKeyString());
+        
+    }
+}
