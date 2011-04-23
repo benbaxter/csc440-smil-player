aca16379fd4fd2c4898fdc65c79e677ee43f0490
baxterb89@gmail.com <baxterb89@gmail.com@1cece00b-e805-e762-627c-26ccc9fbb8d9>
2011-04-23 22:40:13 +0000

    oops

--- branches/trunk/src/com/team1/communication/cloud/CloudConstants.java
+++
@@ -1,7 +0,0 @@
-package com.team1.communication.cloud;
-
-public class CloudConstants
-{
-    public static final String uploadURL = "http://csc440.appspot.com/anupload";
-    public static final String downloadURL = "http://csc440.appspot.com/andownload";
-}
