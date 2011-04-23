6910e2024b22f82105e6793228d51bf71bfa545b
baxterb89@gmail.com <baxterb89@gmail.com@1cece00b-e805-e762-627c-26ccc9fbb8d9>
2011-04-23 04:01:09 +0000

    Merged Jasons file delete work

--- branches/unstable/dev/SMIL Project/src/com/team1/FileBrowserActivity.java
+++ branches/unstable/dev/SMIL Project/src/com/team1/FileBrowserActivity.java
@@ -1,17 +1,14 @@
 package com.team1;
 
 import java.io.File;
-import java.util.ArrayList;
-
-import com.team1.R;
-import com.team1.Smil.SmilConstants;
-import com.team1.player.SmilPlayerActivity;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TreeMap;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ListActivity;
-import android.app.NotificationManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -22,15 +19,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.AdapterView.OnItemClickListener;
+
+import com.team1.Smil.SmilConstants;
+
 
 public class FileBrowserActivity extends ListActivity 
 {
 	private String filePath;
-	private ArrayList<String> smilFiles = new ArrayList<String>();
+	private TreeMap<String, String> smilFilesMap = new TreeMap<String, String>();
 	private Intent in;
 	private Dialog dialog = null;
 	private FileBrowserAdapter adapter;
@@ -74,12 +74,27 @@ public class FileBrowserActivity extends ListActivity
             for ( int index = 0; index < allFiles.length; index++ )
             {
                 if ( allFiles[index].endsWith ( ".smil" ) == true )
-                    smilFiles.add ( allFiles[index] );
+                {
+                    String baseName = allFiles[index];
+                    
+                    String displayName = baseName.substring( 0, baseName.indexOf( "_" ) ) + "\n";
+                    
+                    long milliseconds = Long.parseLong( baseName.substring( baseName.indexOf( "_" ) + 1, baseName.indexOf( "." ) ) );
+                    Calendar time = Calendar.getInstance();
+                    time.setTimeInMillis( milliseconds );
+                    
+                    SimpleDateFormat sdf = new SimpleDateFormat("M/dd hh:mm");
+                    displayName += sdf.format( time.getTime() );
+                    if ( Main.BROWSE_TYPE_DRAFT == type)
+                        smilFilesMap.put( baseName, baseName );
+                    else
+                        smilFilesMap.put( baseName, displayName );
+                }
             }
         }
 
         setContentView ( R.layout.simple_list );
-        adapter = new FileBrowserAdapter ( this, android.R.layout.simple_list_item_1, smilFiles );
+		adapter = new FileBrowserAdapter ( this, smilFilesMap );
         setListAdapter ( adapter );
         
         // Set the title
@@ -106,81 +121,14 @@ public class FileBrowserActivity extends ListActivity
             }
         });
 	}
-	
 	@Override
 	protected void onCreate ( Bundle savedInstanceState ) 
 	{
-	    instance = savedInstanceState;
-		super.onCreate ( savedInstanceState );
-		
-		// Allow for the background to show through when scrolling is happening
-        this.getListView().setCacheColorHint ( Color.TRANSPARENT );
-
-        // Determine where to look (ie Inbox, Outbox, Drafts )
-        File pathDir = Environment.getExternalStorageDirectory ( );
-        File dir = new File ( pathDir, SmilConstants.DRAFT_PATH );
-        
-        in = getIntent ( );
-        Bundle extras = in.getExtras();
-        final int type = extras.getInt( "BROWSE" );
-        
-        if ( in.hasExtra ( "BROWSE" ) )
-        {
-            if ( Main.BROWSE_TYPE_OUTBOX == type)
-            {
-                filePath = SmilConstants.OUTBOX_PATH;
-                dir = new File ( pathDir, filePath );
-            }
-            else if ( Main.BROWSE_TYPE_INBOX == type)
-            {
-                filePath = SmilConstants.INBOX_PATH;
-                dir = new File ( pathDir, filePath );                
-            }
-        }
-
-        filePath = dir.getAbsolutePath().toString();
-        
-		String[] allFiles = dir.list ( );
-		if ( allFiles != null )
-		{
-			// Find all of the .smil files in this directory
-			for ( int index = 0; index < allFiles.length; index++ )
-			{
-				if ( allFiles[index].endsWith ( ".smil" ) == true )
-					smilFiles.add ( allFiles[index] );
-			}
-		}
-
-		setContentView ( R.layout.simple_list );
-		adapter = new FileBrowserAdapter ( this, android.R.layout.simple_list_item_1, smilFiles );
-		setListAdapter ( adapter );
-
-		// Set the title
-        if ( in.hasExtra ( "BROWSE" ) )
-        {
-            TextView tv = (TextView)findViewById ( R.id.Title );
-            
-            if( Main.BROWSE_TYPE_DRAFT == type )
-                tv.setText ( "Drafts" );
-            else if( Main.BROWSE_TYPE_INBOX == type )
-                tv.setText ( "Inbox" );
-            else if( Main.BROWSE_TYPE_OUTBOX == type )
-                tv.setText ( "Drafts" );
-        }
-
-		ListView messageView = getListView ( );
-		
-		messageView.setOnItemClickListener ( new OnItemClickListener ( ) 
-		{
-		    public void onItemClick ( AdapterView<?> parent, View view, int position, long id ) 
-		    {
-		        filePath = filePath + "/" + ((TextView)view).getText().toString();
-		        showDialog( type );
-		    }
-		});
+		this.instance = savedInstanceState;
+		refresh();
 	}
 	
-	protected Dialog onCreateDialog(final int id) {
+	protected Dialog onCreateDialog(int id ) {
         switch (id) {
         case Main.BROWSE_TYPE_DRAFT:
             final String[] items = { "Edit", "Delete" };
@@ -200,10 +148,7 @@ public class FileBrowserActivity extends ListActivity
                     }
                     else if ( item == 1 )
                     {
-                        File file = new File ( filePath );
-                        file.delete ( );
-                        adapter.clearView ( );
-                        refresh ( );
+                        deleteFileAndRefresh( filePath );
                     }
                 }
             });
@@ -219,14 +164,16 @@ public class FileBrowserActivity extends ListActivity
                     Intent data = new Intent();
                     
                     if( item == 0 )
+                    {
                         data.putExtra("ACTION", Main.ACTION_PLAY);
+                        data.putExtra( "FILE", filePath );
+                        
+                        setResult( RESULT_OK, data );
+                        finish();
+                    }
                     else if ( item == 1 )
-                        data.putExtra("ACTION", Main.ACTION_DELETE);
+                        deleteFileAndRefresh( filePath );
                     
-                    data.putExtra( "FILE", filePath );
-
-                    setResult( RESULT_OK, data );
-                    finish();
                 }
             });
             dialog = buildSelecter1.create();
@@ -241,17 +188,26 @@ public class FileBrowserActivity extends ListActivity
                     Intent data = new Intent();
                     
                     if( item == 0 )
+                    {
                         data.putExtra("ACTION", Main.ACTION_PLAY);
-                    else if ( item == 1 )
-                        data.putExtra("ACTION", Main.ACTION_EDIT);
-                    else if ( item == 2 )
-                        data.putExtra("ACTION", Main.ACTION_DELETE);
-                    
-                    data.putExtra( "FILE", filePath );
+                        data.putExtra( "FILE", filePath );
 
-                    setResult( RESULT_OK, data );
-                    finish();
-                }
+                        setResult( RESULT_OK, data );
+                        finish();
+
+                    }
+                    else if ( item == 1 )
+                    {
+                        data.putExtra("ACTION", Main.ACTION_EDIT);
+                        data.putExtra( "FILE", filePath );
+
+                        setResult( RESULT_OK, data );
+                        finish();
+                    }
+                    else if ( item == 2 )
+                        deleteDatabase( filePath );
+                    
+                                    }
             });
             dialog = buildSelecter2.create();
             break;
@@ -259,37 +215,63 @@ public class FileBrowserActivity extends ListActivity
         return dialog;
 	}
 	
-	private class FileBrowserAdapter extends ArrayAdapter<String> 
+	private void deleteFileAndRefresh( String filename )
+    {
+        File file = new File ( filename );
+        file.delete ( );
+        adapter.clearView ( );
+        refresh ( );
+    }
+	
+	private class FileBrowserAdapter extends BaseAdapter 
 	{
-	    private Activity context;
-	    private ArrayList<String> items;
-	    
-	    public FileBrowserAdapter ( Activity context, int textViewId, ArrayList<String> items ) 
-	    {
-	        super ( context, textViewId, items );
-	        this.context = context;
-	        this.items = items;
-	    }
-	    
-	    @Override
-	    public View getView ( int position, View convertView, ViewGroup parent ) 
-	    {
-	        View view = convertView;
-	        if ( view == null ) 
-	        {
-	            LayoutInflater layout = (LayoutInflater)context.getSystemService ( Context.LAYOUT_INFLATER_SERVICE );
-	            view = layout.inflate ( android.R.layout.simple_list_item_1, null );
-	        }
-	        
-	        TextView text = (TextView)view.findViewById ( android.R.id.text1 );
-	        text.setText ( items.get ( position ) );
-	        
-	        return view;
-	    }
+	    private TreeMap<String, String> mData = new TreeMap<String, String>();
+        private String[] mKeys;
+        private Activity context;
+        
+        public FileBrowserAdapter(Activity context, TreeMap<String, String> data){
 
+            this.context = context;
+            mData  = data;
+            mKeys = mData.keySet().toArray(new String[data.size()]);
+        }
+
+        @Override
+        public int getCount() {
+            return mData.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mData.get(mKeys[position]);
+        }
+
+        @Override
+        public long getItemId(int arg0) {
+            return arg0;
+        }
+
+        @Override
+        public View getView(int pos, View convertView, ViewGroup parent) {
+            String key = mKeys[pos];
+            String value = getItem(pos).toString();
+
+            View view = convertView;
+            if ( view == null ) 
+            {
+                LayoutInflater layout = (LayoutInflater)context.getSystemService ( Context.LAYOUT_INFLATER_SERVICE );
+                view = layout.inflate ( android.R.layout.simple_list_item_1, null );
+            }
+            
+            TextView text = (TextView)view.findViewById ( android.R.id.text1 );
+            text.setText ( value );
+            text.setTag( key );
+            
+            return view;
+        }
 	    public void clearView ( ) 
 	    {            
-	        items.clear ( );            
+	        mData.clear ( );            
 	        notifyDataSetChanged ( );        
 	    }
 	}
